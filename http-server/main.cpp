8348585f87729c00ee28fb14d426880dde614fc8
Leonardo Araujo <leonardo.aa88@gmail.com>
2024-04-05 02:23:42 -0300

    some refactor on http server

--- http-server/main.cpp
+++ http-server/main.cpp
@@ -5,19 +5,38 @@
 #include <thread>
 
 namespace beast = boost::beast; // from <boost/beast.hpp>
-namespace http = beast::http; // from <boost/beast/http.hpp>
-namespace net = boost::asio; // from <boost/asio.hpp>
-using tcp = net::ip::tcp; // from <boost/asio/ip/tcp.hpp>
+namespace http = beast::http;   // from <boost/beast/http.hpp>
+namespace net = boost::asio;    // from <boost/asio.hpp>
+using tcp = net::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-void handle_request(http::request<http::string_body>& request, http::response<http::string_body>& response) {
+void handle_request(http::request<http::string_body> &request, http::response<http::string_body> &response)
+{
+    std::cout << request.method_string() << " "
+              << request.target() << " " << request.body() << std::endl;
+
     // Here you would dispatch based on the URI and HTTP method
-    response.result(http::status::ok);
-    response.body() = "Hello, world!";
+    if ((request.method_string()) == "GET" && (request.target() == "/"))
+    {
+        response.result(http::status::ok);
+        response.body() = "Hello, world!";
+    }
+    else if (request.method_string() != "GET")
+    {
+        response.result(http::status::method_not_allowed);
+        response.body() = "Method not allowed";
+    }
+    else
+    {
+        response.result(http::status::ok);
+        response.body() = request.target().substr(1);
+    }
     response.prepare_payload();
 }
 
-void session(tcp::socket socket) {
-    try {
+void session(tcp::socket socket)
+{
+    try
+    {
         beast::flat_buffer buffer;
 
         // Set up an HTTP request parser
@@ -30,16 +49,20 @@ void session(tcp::socket socket) {
 
         // Send the response
         http::write(socket, response);
-    } catch (std::exception const& e) {
+    }
+    catch (std::exception const &e)
+    {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 }
 
-void run_server(short port) {
+void run_server(short port)
+{
     net::io_context io_context{1};
 
-    tcp::acceptor acceptor{io_context, {tcp::v4(), port}};
-    for (;;) {
+    tcp::acceptor acceptor{io_context, {tcp::v4(), static_cast<boost::asio::ip::port_type>(port)}};
+    for (;;)
+    {
         tcp::socket socket{io_context};
         acceptor.accept(socket);
 
@@ -48,12 +71,16 @@ void run_server(short port) {
     }
 }
 
-int main() {
-    try {
-        short port = 8080;
+int main()
+{
+    try
+    {
+        short port = 6969;
         std::cout << "Server starting on port " << port << std::endl;
         run_server(port);
-    } catch (std::exception const& e) {
+    }
+    catch (std::exception const &e)
+    {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 }
